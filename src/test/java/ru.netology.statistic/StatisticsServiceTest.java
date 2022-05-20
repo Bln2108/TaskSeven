8a0c37c2f504919e5fdac1d962b8e37c112ff870
Bln2108 <bln2108@yandex.ru>
2022-05-20 20:45:09 +0300

    100 покрытие

--- src/test/java/ru.netology.statistic/StatisticsServiceTest.java
+++ src/test/java/ru.netology.statistic/StatisticsServiceTest.java
@@ -18,15 +18,15 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
-    //@Test
-    //void findMax1() {
-      //  StatisticsService service = new StatisticsService();
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
 
-        //long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-       // long expected = 12;
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-       // long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-        // assertEquals(expected, actual);
-    //}
+        assertEquals(expected, actual);
+    }
 }
